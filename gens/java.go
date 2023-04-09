324db9978e80088c11fa4f99966aadc1622bf200
Zombi <zombeifedgaming@outlook.com>
2023-04-08 23:27:27 -0500

    Update to java to add the jar builder

--- gens/java.go
+++ gens/java.go
@@ -16,6 +16,50 @@ func GenerateJava(name, ver string) {
 }`, "$pn", name), "$pv", ver))
 	pj.Close()
 
+	jarbuilder, _ := os.Create("Compiler.bat")
+	jarbuilder.WriteString(`@echo off
+
+set "JarName=jarname" 
+set "MainClass=jarclass"
+set "KeepSrcOut=false"
+	
+CLS
+MKDIR "bin"
+DEL "bin\%JarName%.jar" /Q /S
+RMDIR srcOut /Q /S
+Xcopy src srcOut /e/h/c/i
+cd srcOut
+javac *.java
+jar cf "%JarName%.jar" *.class *
+jar --update --verbose --file "%JarName%.jar" --main-class "%MainClass%"
+copy "%JarName%.jar" "../bin/"
+DEL "%JarName%.jar"
+cd ../
+if "%KeepSrcOut%" == "false" RMDIR srcOut /Q /S
+CLS
+echo Compilation is done and your "%JarName%.jar" file is ready sir.
+java -jar "bin/%JarName%.jar"`)
+
+	jarbuilder.Close()
+
+	jbdoc, _ := os.Create("jarbuilder.md")
+	jbdoc.WriteString(`# Jar Compiler
+
+### Config
+batch
+` + "```" + `
+set "JarName=jarname" 
+set "MainClass=jarclass"
+set "KeepSrcOut=false"
+` + "```" + `
+	
+Replace "jarname" with the program name and leave out extentions
+and replace "jarclass" with the class that open the program and leave the program to do the rest.
+
+The required objects to make this work are Compiler.bat and the src folder. The program will create and "srcOut" folder with all the compiled files and pack them into a jar while making a bin folder for the jar to stay in. The Jar file will autorun so you can mess with the finished product and check for errors that may occur in the console/terminal.
+	
+After the jar is created it will delete the srcOut unless you change the KeepSrcOut to true.`)
+
 	os.Mkdir("src", 0700)
 	os.Chdir("src")
 	pf, _ := os.Create(name + ".java")
