4662da00bb3fdf6366499e859eb5d428f2b0718e
Zombi <zombeifedgaming@outlook.com>
2023-04-12 17:09:51 -0500

    Updated to Jar Builder 1.1

--- gens/java.go
+++ gens/java.go
@@ -19,46 +19,81 @@ func GenerateJava(name, ver string) {
 	jarbuilder, _ := os.Create("Compiler.bat")
 	jarbuilder.WriteString(`@echo off
 
-set "JarName=jarname" 
-set "MainClass=jarclass"
-set "KeepSrcOut=false"
+	set "JarName=jarname" 
+	set "MainClass=jarclass"
+	set "KeepSrcOut=false"
 	
-CLS
-MKDIR "bin"
-DEL "bin\%JarName%.jar" /Q /S
-RMDIR srcOut /Q /S
-Xcopy src srcOut /e/h/c/i
-cd srcOut
-javac *.java
-jar cf "%JarName%.jar" *.class *
-jar --update --verbose --file "%JarName%.jar" --main-class "%MainClass%"
-copy "%JarName%.jar" "../bin/"
-DEL "%JarName%.jar"
-cd ../
-if "%KeepSrcOut%" == "false" RMDIR srcOut /Q /S
-CLS
-echo Compilation is done and your "%JarName%.jar" file is ready sir.
-java -jar "bin/%JarName%.jar"`)
+	set "javaDir="
+	set "experiments=false"
+	
+	@REM Only works if experiments are enabled
+	set "javaBaseDir="
+	set "javaJdkVer=jdk-20"
+	
+	
+	
+	@Rem IMPORTANT DO NOT CHANGE
+	set "java=%javaBaseDir%\%javaJdkVer%\bin\"
+	
+	CLS
+	MKDIR "bin"
+	DEL "bin\%JarName%.jar" /Q /S
+	RMDIR srcOut /Q /S
+	Xcopy src srcOut /e/h/c/i
+	cd srcOut
+	if "%experiments%" == "false" "%javaDir%javac" *.java
+	if "%experiments%" == "false" "%javaDir%jar" cf "%JarName%.jar" *.class *
+	if "%experiments%" == "false" "%javaDir%jar" --update --verbose --file "%JarName%.jar" --main-class "%MainClass%"
+	
+	if "%experiments%" == "true" "%java%javac" *.java
+	if "%experiments%" == "true" "%java%jar" cf "%JarName%.jar" *.class *
+	if "%experiments%" == "true" "%java%jar" --update --verbose --file "%JarName%.jar" --main-class "%MainClass%"
+	copy "%JarName%.jar" "../bin/"
+	DEL "%JarName%.jar"
+	cd ../
+	if "%KeepSrcOut%" == "false" RMDIR srcOut /Q /S
+	CLS
+	echo Compilation is done and your "%JarName%.jar" is ready sir.
+	if "%experiments%" == "false" "%java%java" -jar "bin/%JarName%.jar"
+	
+	if "%experiments%" == "true" "%java%java" -jar "bin/%JarName%.jar"`)
 
 	jarbuilder.Close()
 
 	jbdoc, _ := os.Create("jarbuilder.md")
-	jbdoc.WriteString(`# Jar Compiler
+	jbdoc.WriteString(`# Jar Builder ` + "`" + `v1.1` + "`" + `
 
-### Config
-batch
-` + "```" + `
-set "JarName=jarname" 
-set "MainClass=jarclass"
-set "KeepSrcOut=false"
-` + "```" + `
-	
-Replace "jarname" with the program name and leave out extentions
-and replace "jarclass" with the class that open the program and leave the program to do the rest.
+	### Config
 
-The required objects to make this work are Compiler.bat and the src folder. The program will create and "srcOut" folder with all the compiled files and pack them into a jar while making a bin folder for the jar to stay in. The Jar file will autorun so you can mess with the finished product and check for errors that may occur in the console/terminal.
+	` + "```" + `batch
 	
-After the jar is created it will delete the srcOut unless you change the KeepSrcOut to true.`)
+	set "JarName=jarname" 
+	set "MainClass=jarclass"
+	set "KeepSrcOut=false"
+	
+	set "javaDir="
+	` + "```" + `
+	When Using This program leave "javaDir" blank if you want to use your default java used by your system or you could put a custom java dir like ` + "`" + `E:\Program Files\Java\jdk-13\bin\` + "`" + ` for example and make sure your path has ` + "`" + `bin/` + "`" + ` at the end or the program will not work. 
+	
+	Replace "jarname" with the program name and leave out extentions
+	and replace "jarclass" with the class that open the program and leave the program to do the rest.
+	
+	The required objects to make this work are Compiler.bat and the src folder. The program will create and "srcOut" folder with all the compiled files and pack them into a jar while making a bin folder for the jar to stay in. The Jar file will autorun so you can mess with the finished product and check for errors that may occur in the console/terminal.
+	
+	After the jar is created it will delete the srcOut unless you change the KeepSrcOut to true.
+	
+	## Experiments
+
+	*new feature added in Jar Builder 1.1*
+	
+	` + "```" + `batch
+	set "experiments=false"
+	
+	@REM Only works if experiments are enabled
+	set "javaBaseDir="
+	set "javaJdkVer=jdk-13"
+	` + "```" + `
+	Get ready and enable the experiments now we can build with any preinstalled jdk. To use this feature turn "experiments" from false to true and add a "javaBaseDir" which is just where your java "JDKs" are installed then what ever folder your jdk is in take the path and paste it in javaBaseDir and type whatever jdks you have installed in that folder. Java Base Example ` + "`" + `E:\Program Files\Java\` + "`" + `.`)
 
 	os.Mkdir("src", 0700)
 	os.Chdir("src")
